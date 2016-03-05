3f3dfcd40f0641f8ac7daf0aa24d3ba17e1eca6c
Paul Miller <pmiller987@gmail.com>
2016-03-05 16:12:16 -0500

    Reusable submission function.

--- submission.py
+++
@@ -1,2 +0,0 @@
-def submission(preds):
-    pass
---
+++ submitter.py
@@ -0,0 +1,11 @@
+def submit(text):
+    """Pass in the text for the submission.
+    This will save it in a file in /submissions directory.
+    The file name will be submission{x}.csv with x automatically incrementing.
+    """
+    # TODO verify that the file doesn't already exist
+    submission_file_count = len(os.listdir('./submissions'))
+    file_name = 'submission{}.csv'.format(submission_file_count)
+    path = './submissions/{}'.format(file_name)
+    with open(path, 'w') as f:
+        f.write(text)
