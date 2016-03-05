a5ec1d816e13570065818288b90a8b9d14099d4b
Paul Miller <pmiller987@gmail.com>
2016-03-05 16:06:40 -0500

    Make hyperparams optional for sklearn models.

--- sklearn_wrapper.py
+++ sklearn_wrapper.py
@@ -12,12 +12,18 @@ class SkLearnWrapper(AbstractModel):
         super(self.__class__, self).__init__(default_hyperparams, log_level)
 
 
-    def fit(self, data, targets, hyperparams):
-        if not len([self.model]):
-            self.logger.error('No model assigned')
-            raise Exception('No model')
+    def fit(self, data, targets, hyperparams={}):
+        if not self.model:
+            msg = 'No model assigned. You probably need to run .tune() first.'
+            self.logger.warning(msg)
+            self._initialize_model(hyperparams)
+
         if hyperparams:
             self.model.set_params(**hyperparams)
+        else:
+            msg = """No model assigned. You need to run .tune()
+                     first or manually pass in hyperparams."""
+            self.logger.warning(msg)
         self.model.fit(data, targets)
 
 
